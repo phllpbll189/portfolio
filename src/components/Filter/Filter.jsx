ab22ced53aa566a583e13705b2735a0080b6ad71
phllpbll189 <phillip@devbell.net>
2024-04-17 15:49:50 -0500

    starts the process of animating the filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,16 +1,27 @@
+import { useState } from 'react'
 import './Filter.css'
 
 export const Filter = ({ selection, setSelection}) => {
+    const [filterVisible, setVisibility] = useState(false)
+
     let select = (lang) => {
         setSelection({...selection, [lang]: !selection[lang]})
     }
 
+
+    //need to learn how to do react transitions to get desired effect
+    //if I want to animate the filter
+
+    //this is because everytime it changes it completely reloads the root.
     return(
         <>
-            <div>
-                <button onClick={() => select("Javascript")}>Javascript</button>
-                <button onClick={() => select("C++")}>C++</button>
-                <button onClick={() => select("Java")}>Java</button> 
+            <div className='filter'>
+                <p onClick={() => setVisibility(!filterVisible)}>Filter</p>
+                <div>
+                    <button onClick={() => select("Javascript")}>Javascript</button>
+                    <button onClick={() => select("C++")}>C++</button>
+                    <button onClick={() => select("Java")}>Java</button> 
+                </div>
             </div>
         </>
     )
