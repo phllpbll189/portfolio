6f053a5f2978af0299d16a7ca1a71c968a3fa425
phllpbll189 <phillip@devbell.net>
2024-04-27 21:02:06 -0500

    fixes navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ export const Navbar = () => {
 
     return(
         <div className="navbar">
-            <span href="/">Home</span>
-            <span href="/portfolio" className='disabled'><a href='/portfolio'>Portfolio</a></span>
+            <span href="/"><a href='/' style={{color: "inherit", textDecoration: 'none'}}>Home</a></span>
+            <span href="/portfolio"><a href='/portfolio' style={{color: "inherit", textDecoration: 'none'}}>Portfolio</a></span>
             <span>Hire</span>
         </div>
     );
